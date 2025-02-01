877e81b8b25a79efd90ddcdb7d89d8075c3515de
Harman Khangura <harman198@hotmail.com>
2025-02-01 21:15:39 +1000

    Added conditionals

---
+++ conditional.fsx
@@ -0,0 +1,26 @@
+open System.IO
+
+let processFile (fileName: string) = 
+    let fileExtension  = Path.GetExtension(fileName)
+
+    if fileExtension = ".fs" then
+        printfn "This is a source file"
+    elif fileExtension = ".fsx" then
+        printfn "This is a script"
+    elif fileExtension = ".fsproj" then
+        printfn "This is a build configuration file"
+    else printfn "Can't process file"
+
+processFile "hello.fsx"
+processFile "app.fs"
+processFile "README.md"
+
+let divide x y = 
+    try
+        Some (x/y)
+    with 
+        | :? System.DivideByZeroException -> printfn "Can't divide by zero"; None
+        | ex -> printfn "Some other exception occurred"; None
+
+divide 10 2
+divide 1 0
